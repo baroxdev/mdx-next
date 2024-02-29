21a15310cda696fe4f49bf73e885200b1a7106c1
baroxdev <105564773+ThanhPhong31@users.noreply.github.com>
2024-02-29 10:15:38 +0700

    Update next.config.mjs with static exports and disable server-based image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/mdx-next',
+  /**
+   * Enable static exports for the App Router.
+   *
+   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
   output: 'export',
+
+  /**
+   * Set base path. This is usually the slug of your repository.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: '/mdx-next',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/pages/api-reference/components/image#unoptimized
+   */
   images: {
-    domains: ['github.com', 'lh3.googleusercontent.com']
-  },
-  experimental: {
-    appDir: true
+    unoptimized: true
   }
 }
 
