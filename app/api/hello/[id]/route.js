4028797b73a3e7c3ff9a13255acd94ff0fc582a5
baroxdev <105564773+ThanhPhong31@users.noreply.github.com>
2024-02-29 08:43:00 +0700

    Delete unused files and components

--- app/api/hello/[id]/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET(request, { params }) {
-  const id = params.id
-  const { searchParams } = request.nextUrl
-  const sort = searchParams.get('sort')
-  return NextResponse.json({ id, sort }, { status: 201 })
-}
