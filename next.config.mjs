843a2c7382af6a34ce30553e57487c7f2c2f1f0e
baroxdev <105564773+ThanhPhong31@users.noreply.github.com>
2024-02-29 08:52:34 +0700

    Add basePath and output options to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: '/mdx-next',
+  output: 'export',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
     appDir: true
