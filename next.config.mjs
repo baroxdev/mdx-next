eedf93ded53cfc427091b0478b95d867afa3675f
baroxdev <105564773+ThanhPhong31@users.noreply.github.com>
2024-02-29 10:06:45 +0700

    Fix bug in login functionality

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  generateEtags: false,
   basePath: '/mdx-next',
   output: 'export',
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   experimental: {
     appDir: true
   },
-  // assetPrefix: './',
   images: {
     domains: ['github.com', 'lh3.googleusercontent.com']
   }
